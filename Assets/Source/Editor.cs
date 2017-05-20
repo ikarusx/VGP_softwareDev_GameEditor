2337c04aa52151f471452f0485c8786d2716b526
DasApratim <394504@AIV.EDMC.STU>
2017-05-19 20:37:39 -0700

    Duong's workshop change

---
+++ Assets/Source/Editor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Editor : MonoBehaviour {
+
+    public Level mLevelPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void createLevel()
+    {
+        // creating new template level object from mLevelPrefab
+    }
+}
---
+++ Assets/Source/Level.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Level : MonoBehaviour {
+    public Hud mHud;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
