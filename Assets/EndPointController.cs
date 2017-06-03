ddb3a33789073a88012647bd0bb2d2d098ad2a00
EscobarJoseph Alfonso <eeveeday@gmail.com>
2017-06-02 18:58:26 -0700

    simplified it and use inheritance instead

---
+++ Assets/EndPointController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndPointController : MonoBehaviour {
+
+    private bool invoked = false;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D thing)
+    {
+        Debug.Log("collider tag = " + thing.gameObject.tag);
+        if (thing.gameObject.tag != "Player") return;
+        if (invoked) return;
+
+        invoked = true;
+        // Unity 5.3.5 only uses NET 4, so OnCollisionEvent?.Invoke() is invalid.
+        OnEnding();
+    }
+
+    void OnEnding()
+    {
+        Debug.Log("Level complete!");
+    }
+}
