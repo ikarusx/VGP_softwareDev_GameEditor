bebbbe0b1194d1c2ece642e91b7ac8a82786f396
Frank Xu <jxu129@gmail.com>
2017-06-09 17:22:30 -0700

    finish save and load

---
+++ Assets/Ikarusx/Scripts/LoadPrefab.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEditor;
+
+public class LoadPrefab : MonoBehaviour
+    , IPointerClickHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+{
+    public GameObject SaveFunction;
+    public GameObject CameraFunction;
+    public GameObject level;
+
+    private SavePrefab SaveScript;
+    private MoveCameraIk CameraScript;
+
+    void Awake()
+    {
+        SaveScript = SaveFunction.GetComponent<SavePrefab>();
+        CameraScript = CameraFunction.GetComponent<MoveCameraIk>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Load()
+    {
+        Destroy(level);
+        GameObject go = Instantiate(Resources.Load("Level",  typeof(GameObject))) as GameObject;
+        level = go;
+        SaveScript.level = go;
+        GameObject newplayer = go.transform.Find("Player").gameObject;
+        if(newplayer != null)
+        {
+            print("Found new player");
+            CameraScript.player = newplayer;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("click load");
+        Load();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+}
---
+++ Assets/Ikarusx/Scripts/SavePrefab.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEditor;
+
+public class SavePrefab : MonoBehaviour
+    , IPointerClickHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+{
+    public GameObject level;
+
+    void Start()
+    {
+    }
+
+    void Save()
+    {
+        PrefabUtility.CreatePrefab("Assets/Resources/Level.prefab", level);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("click save");
+        Save();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+}
